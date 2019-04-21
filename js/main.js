ad2f8df9adc3d36729ed3ce15caa63903ec7c469
destanol <39131893+destanol@users.noreply.github.com>
2019-04-21 10:10:18 -0400

    CSS + Some Menu Functionality

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 
 var toggle = document.querySelector('#btnToggle');
 var menu = document.querySelector('.menu');
-var menuItem = document.querySelector('.menu');
-// var navHeight = document.querySelector('.')1
+var navItem = document.querySelectorAll('.navItem a');
 
 
 function openNav() {
   menu.style.display = 'flex';
-  toggleBtn.style.fill= 'navItem';
-};
+}
 
 function closeNav() {
   menu.style.display = 'none';
-
-};
+}
 
 function toggleNav() {
   if (menu.style.display === 'none')
-    {openNav()
+    { openNav();
   } else
-    {closeNav()
+    { closeNav();
   }
 }
 
-toggle.addEventListener('click', toggleNav());
+toggle.addEventListener('click', toggleNav);
 
-navItem.addEventListener('click'), toggleNav();
+navItem.addEventListener('click', toggleNav);
